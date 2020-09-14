2c6f670972aba906b7f47e756d8a6516c56a7bf9
Kristian Kostov <k.dimitroff2@gmail.com>
2020-09-14 09:28:03 +0100

    Initial Commit of GoScrapey

---
+++ cmd/scrapey.go
@@ -0,0 +1,26 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "scrapey",
+	Short: "GoScrapey is a website image scraper",
+	Long: `An image scraper build with love by Brizinger in Go
+				  More information can be found on http://github.com/brizinger/GoScrapey`,
+	Run: func(cmd *cobra.Command, args []string) {
+		// Do Stuff Here
+	},
+}
+
+// Execute - executes the command
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/brizinger/goscrapey/cmd"
+)
+
+func main() {
+	cmd.Execute()
+}
